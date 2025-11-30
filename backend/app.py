96107f5c2eed7e2abd4a88f4cae17daa642a2395
Ravikant Raj <ravikantraj384@gmail.com>
2025-11-30 20:45:32 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,18 @@
+from flask import Flask, request, jsonify
+from routes.recommend import get_recommendation
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "EcoPackAI Backend Running"}), 200
+
+@app.route("/recommend", methods=["POST"])
+def recommend():
+    data = request.json
+    result = get_recommendation(data)
+    return jsonify(result), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
